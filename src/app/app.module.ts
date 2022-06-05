9b72a98023e77c2d44667c700aa84bc2cdfc1a1c
Gianni Fontanot <giannifontanot@gmail.com>
2022-06-04 22:01:40 -0500

    Filter working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {ProductListComponent} from "./products/product-list.component";
+import {ConvertToSpacesPipe} from "./shared/convert-to-spaces-pipe";
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    ConvertToSpacesPipe
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProduct} from './IProduct';
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'pm-products',
@@ -7,12 +8,24 @@ import {IProduct} from './IProduct';
   styleUrls: ['./product-list.component.css']
 
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   showImage: boolean = false;
   pageTitle: string = 'Product List! ';
   imageWidth: number = 50;
   imageMargin: number = 2;
-  listFilter: string = 'cart';
+
+  private _listFilterx: string = "";
+
+  get listFilter(): string {
+    return this._listFilterx;
+  }
+
+  set listFilter(value: string) {
+    console.log(value);
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
   products: IProduct[] = [
     {
       "productId": 2,
@@ -34,7 +47,19 @@ export class ProductListComponent {
       "starRating": 4.8,
       "imageUrl": "assets/images/hammer.png"
     }];
+
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(keyword: string): IProduct[] {
+    keyword = keyword.toLocaleLowerCase()
+    return this.products.filter((oneProduct: IProduct) => oneProduct.productName.toLocaleLowerCase().includes(keyword));
+    // return this.products.filter((oneProduct: IProduct) => {return true;});
+  }
+
+
+  ngOnInit(): void {
+    this._listFilterx = 'Hammer';
   }
 }

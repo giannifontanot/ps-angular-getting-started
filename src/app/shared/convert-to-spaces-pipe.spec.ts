c1011f3296bc9fb9f894b3d7a6b22336b3cfdb09
Gianni Fontanot <giannifontanot@gmail.com>
2022-06-04 22:00:15 -0500

    Custom Pipe created

---
+++ src/app/shared/convert-to-spaces-pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ConvertToSpacesPipe } from './convert-to-spaces-pipe';
+
+describe('ConvertToSpacesPipe', () => {
+  it('should create an instance', () => {
+    expect(new ConvertToSpacesPipe()).toBeTruthy();
+  });
+});

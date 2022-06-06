ced090309178213610577c5fb69e87a71f6e59f9
Gianni Fontanot <giannifontanot@gmail.com>
2022-06-05 21:02:45 -0500

    Service created and injected

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {IProduct} from './IProduct';
-import {filter} from "rxjs/operators";
+import {ProductService} from "./product.service";
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
-
+  styleUrls: ['./product-list.component.css'],
+  providers: [ProductService]
 })
 
 export class ProductListComponent implements OnInit {
@@ -14,8 +14,12 @@ export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List! ';
   imageWidth: number = 50;
   imageMargin: number = 2;
-
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
   private _listFilterx: string = "";
+
+  constructor(private productService: ProductService) {
+  }
 
   get listFilter(): string {
     return this._listFilterx;
@@ -26,29 +30,6 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.performFilter(value);
   }
 
-  filteredProducts: IProduct[] = [];
-  products: IProduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }];
-
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
@@ -56,15 +37,16 @@ export class ProductListComponent implements OnInit {
   performFilter(keyword: string): IProduct[] {
     keyword = keyword.toLocaleLowerCase()
     return this.products.filter((oneProduct: IProduct) => oneProduct.productName.toLocaleLowerCase().includes(keyword));
-    // return this.products.filter((oneProduct: IProduct) => {return true;});
   }
 
-
-  onNotify($event: any):void {
-  this.pageTitle = $event;
-  }
 
   ngOnInit(): void {
-    this._listFilterx = 'Hammer';
+    this.products = this.productService.getPoducts()
+    this.filteredProducts = this.products;
+    // this._listFilterx = 'Hammer';
+  }
+
+  onNotify($event: any): void {
+    this.pageTitle = $event;
   }
 }

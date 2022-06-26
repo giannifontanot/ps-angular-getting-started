ad30f9999691e9a0a41bf9c4baddd7a7cef7d825
Gianni Fontanot <giannifontanot@gmail.com>
2022-06-26 08:18:28 -0500

    Guard implemented for product-detail

---
+++ src/app/products/product-detail.guard.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDetailGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const id = Number(route.paramMap.get('id'));
+    if (isNaN(id) || id < 1) {
+      alert('Invalid product id');
+      return false;
+    }
+    return true;
+  }
+
+}
